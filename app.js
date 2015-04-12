5d4bceab472c0ef2f26d6c8ad510fbeda38344f5
Lolobstant <lolokun@gmail.com>
2015-04-12 15:43:02 +0200

    suppression indentation en trop

--- app.js
+++ app.js
@@ -1,120 +1,120 @@
 (function (exports) {
-		"use strict";
+	"use strict";
 
-		var api = {
-				run: _run
-		};
-		extend(exports, api);
+	var api = {
+		run: _run
+	};
+	extend(exports, api);
 
-		var app         = require('express')(),
-		    morgan      = require("morgan"),
-		    cors        = require('cors'),
-		    busboy      = require('connect-busboy'),
-		    Q           = require('q'),
-		    defaultConf = {
-				    port:         8080,
-				    provider:     "filesystem",
-				    providerConf: {}
-		    },
-		    provider;
+	var app         = require('express')(),
+	    morgan      = require("morgan"),
+	    cors        = require('cors'),
+	    busboy      = require('connect-busboy'),
+	    Q           = require('q'),
+	    defaultConf = {
+		    port:         8080,
+		    provider:     "filesystem",
+		    providerConf: {}
+	    },
+	    provider;
 
-		function extend(obj) {
-				var args = [].slice.call(arguments, 1);
-				args.forEach(function (source) {
-						for (var prop in source) {
-								if (source.hasOwnProperty(prop))
-										obj[prop] = source[prop];
-						}
-				});
-				return obj;
+	function extend(obj) {
+		var args = [].slice.call(arguments, 1);
+		args.forEach(function (source) {
+			for (var prop in source) {
+				if (source.hasOwnProperty(prop))
+					obj[prop] = source[prop];
+			}
+		});
+		return obj;
+	}
+
+	function _run(userConf) {
+		var conf = merge(defaultConf, userConf);
+
+		provider = getProvider(conf.provider);
+		app.use([
+			morgan("dev"),
+			busboy(),
+			cors()
+		]);
+
+		app.post("*", function (req, res) {
+			var seq;
+			if (req.headers["content-type"].indexOf('multipart/form-data') != -1)
+				seq = upload_multipart(req);
+			else
+				seq = upload_raw(req);
+			seq.then(function (data) {
+				res.status(200).send('very put');
+			}, function (err) {
+				console.log("err : ", err);
+				res.status(404).send('not found =(');
+			});
+		});
+
+		app.get("*", function (req, res) {
+			provider.get(req.url.substring(1)).then(function (stream) {
+				stream.pipe(res.status(200));
+			}, function (err) {
+				res.status(404).send('not found =(');
+			});
+		});
+
+		app.listen(conf.port, function () {
+			console.log("running on port " + conf.port);
+		});
+	}
+
+	function upload_multipart(req) {
+		var deferred = Q.defer();
+		try {
+			req.pipe(req.busboy);
+			req.busboy.on('file', function (fieldname, stream, filename) {
+				console.log('to put : ', filename);
+				provider.put(stream, filename).then(deferred.resolve, deferred.reject);
+			});
 		}
-
-		function _run(userConf) {
-				var conf = merge(defaultConf, userConf);
-
-				provider = getProvider(conf.provider);
-				app.use([
-						morgan("dev"),
-						busboy(),
-						cors()
-				]);
-
-				app.post("*", function (req, res) {
-						var seq;
-						if (req.headers["content-type"].indexOf('multipart/form-data') != -1)
-								seq = upload_multipart(req);
-						else
-								seq = upload_raw(req);
-						seq.then(function (data) {
-								res.status(200).send('very put');
-						}, function (err) {
-								console.log("err : ", err);
-								res.status(404).send('not found =(');
-						});
-				});
-
-				app.get("*", function (req, res) {
-						provider.get(req.url.substring(1)).then(function (stream) {
-								stream.pipe(res.status(200));
-						}, function (err) {
-								res.status(404).send('not found =(');
-						});
-				});
-
-				app.listen(conf.port, function () {
-						console.log("running on port " + conf.port);
-				});
+		catch (e) {
+			deferred.reject(e);
 		}
+		return deferred.promise;
+	}
 
-		function upload_multipart(req) {
-				var deferred = Q.defer();
-				try {
-						req.pipe(req.busboy);
-						req.busboy.on('file', function (fieldname, stream, filename) {
-								console.log('to put : ', filename);
-								provider.put(stream, filename).then(deferred.resolve, deferred.reject);
-						});
+	function upload_raw(req, res) {
+		return provider.put(req);
+	}
+
+	function merge(source, mix) {
+		var res = source;
+		for (var i in mix) {
+			if (mix.hasOwnProperty(i) && i in source)
+				res[i] = mix[i];
+		}
+		return res;
+	}
+
+	function getProvider(providerName) {
+		var provider;
+		try {
+			provider = require("./providers/" + providerName);
+			if (typeof provider.get !== 'function' ||
+				typeof provider.put !== 'function' ||
+				typeof provider.setConf !== 'function')
+				throw new Error('Provider: ' + providerName + ' malconstruct');
+			provider.setConf(defaultConf.providerConf);
+		} catch (e) {
+			provider = {
+				get: function () {
+					throw new Error('Invalid provider');
+				},
+				put: function () {
+					throw new Error('Invalid provider');
 				}
-				catch (e) {
-						deferred.reject(e);
-				}
-				return deferred.promise;
+			};
+			console.error(e);
 		}
-
-		function upload_raw(req, res) {
-				return provider.put(req);
-		}
-
-		function merge(source, mix) {
-				var res = source;
-				for (var i in mix) {
-						if (mix.hasOwnProperty(i) && i in source)
-								res[i] = mix[i];
-				}
-				return res;
-		}
-
-		function getProvider(providerName) {
-				var provider;
-				try {
-						provider = require("./providers/" + providerName);
-						if (typeof provider.get !== 'function' ||
-								typeof provider.put !== 'function' ||
-								typeof provider.setConf !== 'function')
-								throw new Error('Provider: ' + providerName + ' malconstruct');
-						provider.setConf(defaultConf.providerConf);
-				} catch (e) {
-						provider = {
-								get: function () {
-										throw new Error('Invalid provider');
-								},
-								put: function () {
-										throw new Error('Invalid provider');
-								}
-						};
-						console.error(e);
-				}
-				return provider;
-		}
+		return provider;
+	}
 
 })(module.exports);
--- providers/filesystem.js
+++ providers/filesystem.js
@@ -1,73 +1,73 @@
 (function (exports) {
-		"use strict";
-		var Q    = require('q'),
-		    fs   = require('fs'),
-		    path = require('path');
-		var defaultPath = path.resolve(__dirname, '../tmp/');
-		if (!fs.existsSync(defaultPath))
-			fs.mkdirSync(defaultPath);
-		var api         = {
-				    get:     _get,
-				    put:     _put,
-				    setConf: _setConf
-		    },
-		    defaultConf = {
-				    path: defaultPath
-		    };
-		extend(exports, api);
+	"use strict";
+	var Q    = require('q'),
+	    fs   = require('fs'),
+	    path = require('path');
+	var defaultPath = path.resolve(__dirname, '../tmp/');
+	if (!fs.existsSync(defaultPath))
+		fs.mkdirSync(defaultPath);
+	var api         = {
+		    get:     _get,
+		    put:     _put,
+		    setConf: _setConf
+	    },
+	    defaultConf = {
+		    path: defaultPath
+	    };
+	extend(exports, api);
 
-		function extend(obj) {
-				var args = [].slice.call(arguments, 1);
-				args.forEach(function (source) {
-						for (var prop in source) {
-								if (source.hasOwnProperty(prop))
-										obj[prop] = source[prop];
-						}
-				});
-				return obj;
+	function extend(obj) {
+		var args = [].slice.call(arguments, 1);
+		args.forEach(function (source) {
+			for (var prop in source) {
+				if (source.hasOwnProperty(prop))
+					obj[prop] = source[prop];
+			}
+		});
+		return obj;
+	}
+
+	function _get(fileName) {
+		return Q.Promise(function (resolve, reject) {
+			var filePath = path.resolve(defaultConf.path, fileName)
+			var stream = fs.createReadStream(filePath);
+			stream.on("error", function (error) {
+				reject(error);
+			});
+			stream.on("readable", function () {
+				resolve(stream);
+			});
+		});
+	}
+
+	function _put(stream, fileName) {
+		if (!fileName)
+			fileName = "noname.jpg" // do cool stuff here
+
+		console.log("very put");
+		var filePath = path.resolve(defaultConf.path, fileName)
+		return Q.Promise(function (resolve, reject) {
+			try {
+				console.log('path :', filePath, fileName);
+				var file = fs.createWriteStream(filePath);
+				stream.pipe(file);
+				stream.on('error', reject);
+				file.on('error', reject)
+					.on("finish", function () {
+						resolve("file uploaded");
+					});
+			} catch (e) {
+				console.log(e);
+				reject(e);
+			}
+		});
+	}
+
+	function _setConf(userConf) {
+		for (var i in userConf) {
+			if (userConf.hasOwnProperty(i) && i in defaultConf)
+				defaultConf[i] = userConf[i];
 		}
-
-		function _get(fileName) {
-				return Q.Promise(function (resolve, reject) {
-						var filePath = path.resolve(defaultConf.path, fileName)
-						var stream = fs.createReadStream(filePath);
-						stream.on("error", function (error) {
-								reject(error);
-						});
-						stream.on("readable", function () {
-								resolve(stream);
-						});
-				});
-		}
-
-		function _put(stream, fileName) {
-				if (!fileName)
-						fileName = "noname.jpg" // do cool stuff here
-
-				console.log("very put");
-				var filePath = path.resolve(defaultConf.path, fileName)
-				return Q.Promise(function (resolve, reject) {
-						try {
-								console.log('path :', filePath, fileName);
-								var file = fs.createWriteStream(filePath);
-								stream.pipe(file);
-								stream.on('error', reject);
-								file.on('error', reject)
-										.on("finish", function () {
-												resolve("file uploaded");
-										});
-						} catch (e) {
-								console.log(e);
-								reject(e);
-						}
-				});
-		}
-
-		function _setConf(userConf) {
-				for (var i in userConf) {
-						if (userConf.hasOwnProperty(i) && i in defaultConf)
-								defaultConf[i] = userConf[i];
-				}
-		}
+	}
 
 })(module.exports);
